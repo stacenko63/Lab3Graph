bb84da4a4e282af49ff94127f08d6ccd9a3d950d
stacenko63 <amr_stacenko@mail.ru>
2022-05-05 21:47:24 +0400

    fix: fix errors in create graph

--- Study/Source.cpp
+++ Study/Source.cpp
@@ -3,6 +3,8 @@
 #include <list>
 #include <algorithm>
 #include <queue>
+#include <limits>
+#include <iterator>
 using namespace std;
 
 //Вариант 1: 0·2^1 + 1·2^0 = 01    (обход в ширину и алгоритм Беллмана-Форда) 
@@ -42,7 +44,7 @@ public:
 };
 
 ostream& operator<<(ostream& out, const Locality& locality) {
-	out << locality.get_name() << " " << locality.get_population(); 
+	out << "Name = " << locality.get_name() << " Population = " << locality.get_population();
 	return out; 
 }
 
@@ -66,14 +68,15 @@ struct Vertex {
 };
 
 ostream& operator<<(ostream& out, const Edge& edge) {
-	out << "( " << edge.value << ", " << edge.length << " )";
+	out << "( " << edge.value << ", Length = " << edge.length << " )";
 	return out;
 }
 
 ostream& operator<<(ostream& out, const Vertex& vertex) {
 	out << vertex.locality << " : ";
-	for (auto el : vertex.edges) {
-		cout << el << " , ";
+	for (auto it = begin(vertex.edges); it != end(vertex.edges); ++it) {
+		cout << *it;
+		if (next(it) != end(vertex.edges)) cout << " -> ";
 	}
 	return out; 
 }
@@ -143,52 +146,83 @@ public:
 		auto it = check_vertexes_existence(from, to); 
 		Vertex& result = *it; 
 		auto it_edge = check_edge_existence(result, to);
-		if (it_edge != end(result.edges))
-			throw "Эти ребра уже итак смежны!";
+		if (it_edge == end(result.edges))
+			throw "Эти ребра уже итак несмежны!";
 		result.edges.erase(it_edge); 
 	}
 
 	void print() const {
 		for (auto el : table) {
-			cout << el << ":  ";
-			for (auto element : el.edges) {
-				cout << element << " ";
-			}
-			cout << "\n";
+			cout << el << "\n\n\n";
 		}
 	}
 
-	void traversing_in_width(const Locality& locality) {
-		queue<Vertex&> q;
-		auto it = check_vertex_existence(locality);
-		if (it == end(table)) 
-			throw "Указанная вершина отсутствует!"; 
-		Vertex& result = *it; 
-		q.push(result);  
-		do {
-			result = q.back(); 
-			q.pop();
-			result.check = true; 
-			for (auto el : result.edges) {
-				auto element = check_vertex_existence(el.value);
-				Vertex& tmp = *element;
-				if (!tmp.check) q.push(tmp); 
-			} 
-		} while (!q.empty()); 
-		change_flags();  
-	}
+	//void traversing_in_width(const Locality& locality) {
+	//	queue<Vertex&> q;
+	//	auto it = check_vertex_existence(locality);
+	//	if (it == end(table)) 
+	//		throw "Указанная вершина отсутствует!"; 
+	//	Vertex& result = *it; 
+	//	q.push(result);  
+	//	do {
+	//		result = q.back(); 
+	//		cout << result.locality << " , ";
+	//		q.pop();
+	//		result.check = true; 
+	//		for (auto el : result.edges) {
+	//			auto element = check_vertex_existence(el.value);
+	//			Vertex& tmp = *element;
+	//			if (!tmp.check) q.push(tmp); 
+	//		} 
+	//	} while (!q.empty()); 
+	//	change_flags();  
+	//}
 
-	list<Vertex> get_latest_way(const Locality& from, const Locality& to) {
-		auto it = check_vertexes_existence(from, to);
-		list<Vertex> result;
-		 
+	//struct str {
+	//	double v;
+	//	str* prev;  
+	//};
 
-		return result;
-	}
+	//list<Vertex> get_latest_way(const Locality& from, const Locality& to) { //алгоритм Беллмана-Форда
+	//	auto it = check_vertexes_existence(from, to);
+	//	list<Vertex> result {*it};
+	//	vector<vector<double>> v;
+	//	v.resize(table.size());
+	//	for (int i = 0; i < table.size(); i++) {
+	//		v[i].resize(table.size());
+	//	}
+	//	for (int i = 0; i < table.size(); i++) {
+	//		v[0][i] = numeric_limits<int>::infinity();
+	//	}
+	//	v[0][it-begin(table)] = 0; 
+	//	for (int i = 1; i < table.size(); i++) {
+	//		for (int j = 0; j < table.size(); j++) {
+	//			v[i][j] = table[i].edges[];
+	//		}
+	//	}
+	//	reverse(begin(result), end(result)); 
+	//	return result;
+	//}
 };
 
 
 int main() {
 	setlocale(LC_ALL, "RUS"); 
-	RoadNetwork rn; 
+	try {
+		RoadNetwork rn;
+		rn.add_vertex(Locality("Samara", 1000000));
+		rn.add_vertex(Locality("Moscow", 2000000));
+		rn.add_vertex(Locality("Saint-Peterburg", 10000));
+		rn.add_vertex(Locality("Sochi", 100));
+
+		rn.add_edge(Locality("Samara", 1000000), Locality("Moscow", 2000000), 350);
+		rn.add_edge(Locality("Sochi", 100), Locality("Saint-Peterburg", 10000), 200);
+
+		rn.print();  
+
+	}
+	catch (const char* ex) {
+		cout << ex;  
+	}
+	
 }
