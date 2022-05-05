5ee38c10d0d47ff0340f901b6e0f9975fc92c7bf
stacenko63 <amr_stacenko@mail.ru>
2022-05-05 14:51:05 +0400

    feat: add del_vertex and del_edge

--- Study/Source.cpp
+++ Study/Source.cpp
@@ -14,7 +14,7 @@ private:
 
 public:
 
-	Locality(string name, int population) {
+	Locality(const string& name, int population) {
 		set_name(name);
 		set_population(population); 
 	}
@@ -24,7 +24,7 @@ public:
 		this->population = population; 
 	}
 
-	int set_name(string name) {
+	int set_name(const string& name) {
 		if (name.size() == 0) throw "Ќазвание населенного пункта не должно быть пустым!";
 		this->name = name;
 	}
@@ -48,65 +48,112 @@ bool operator==(const Locality& lhs, const Locality& rhs) {
 	return lhs.get_name() == rhs.get_name() && lhs.get_population() == rhs.get_population(); 
 }
 
+
+
+struct Edge {
+	Edge(const Locality& locality, double Length = 1) : value(locality), length(Length) {}
+	Locality value;
+	double length;
+};
+
+struct Vertex {
+	Vertex(const Locality& Locality) : locality(Locality) {}
+	Locality locality;
+	list<Edge> edges;
+};
+
+ostream& operator<<(ostream& out, const Edge& edge) {
+	out << "( " << edge.value << ", " << edge.length << " )";
+	return out;
+}
+
+ostream& operator<<(ostream& out, const Vertex& vertex) {
+	out << vertex.locality << " : ";
+	for (auto el : vertex.edges) {
+		cout << el << " , ";
+	}
+	return out; 
+}
+
+
+
+
+
 class RoadNetwork {
 	
 private:
-	
-	struct Edge {
-		int value; 
-		double lenght = 1; 
-	};
-
-	struct Vertex {
-		Vertex(const Locality& Locality): locality(Locality) {}
-		Locality locality; 
-		list<Edge> edges;  
-	};
 
 	vector<Vertex> table;  
 
-public:
+	vector<Vertex>::iterator check_vertexes_existence(const Locality& first, const Locality& second) {
+		auto it = check_vertex_existence(first);
+		if (it == end(table) || check_vertex_existence(second) == end(table)) {
+			throw "ƒанные вершины отсутствуют!";
+		}
+		return it; 
+	}
 
-	auto check_vertex_existence(const Locality& locality) {
+	vector<Vertex>::iterator check_vertex_existence(const Locality& locality) {
 		return find(begin(table), end(table), [locality](const Vertex& vertex) {
-			return vertex.locality == locality; 
+			return vertex.locality == locality;
 			});
 	}
 
+	list<Edge>::const_iterator check_edge_existence(const Vertex& vertex, const Locality& locality) {
+		return find(begin(vertex.edges), end(vertex.edges), [locality](const Edge& edge) {
+			return locality == edge.value;
+			});
+	}
+
+public:
+
+
+
 	void add_vertex(const Locality& locality) {
-		if (find(begin(table), end(table), locality) == end(table)) 
+		if (find(begin(table), end(table), locality) != end(table)) 
 			throw "”казанна€ вершина уже присутствует!";
-		table.push_back(locality); 
+		table.push_back(Vertex(locality)); 
 	}
 
 	void add_edge(const Locality& from, const Locality& to, double length) {
-		auto it = check_vertex_existence(from); 
-		if (it == end(table) || check_vertex_existence(to) == end(table)) {
-			throw "ƒанные вершины отсутствуют!";
-		}
-		it.
+		auto it = check_vertexes_existence(from, to);
+		Vertex& result = *it; 
+		if (check_edge_existence(result, to) != end(result.edges))
+			throw "Ёти ребра уже итак смежны!";
+		result.edges.push_back(Edge(to, length)); 
 	}
+
+	void del_vertex(const Locality& locality) {
+		auto it = check_vertex_existence(locality);
+		if (it == end(table))
+			throw "”казанна€ вершина уже отсутствует!";
+		table.erase(it);
+	}
+
+	void del_edge(const Locality& from, const Locality& to) {
+		auto it = check_vertexes_existence(from, to); 
+		Vertex& result = *it; 
+		auto it_edge = check_edge_existence(result, to);
+		if (it_edge != end(result.edges))
+			throw "Ёти ребра уже итак смежны!";
+		result.edges.erase(it_edge); 
+	}
+
+	void print() {
+		for (auto el : table) {
+			cout << el << ":  ";
+			for (auto element : el.edges) {
+				cout << element << " ";
+			}
+			cout << "\n";
+		}
+	}
+
+
 };
 
 
 int main() {
 	setlocale(LC_ALL, "RUS"); 
-	/*vector<int> v = { 1,2,3,4,5 };
-	for (auto el : v) {
-		cout << el << " ";
 
-	}
-	cout << "\n\n\n";
-	auto it = find(begin(v), end(v), 95);
-	if (it == end(v)) {
-		cout << "!!!!!!";
-	}
-	else {
-		*it = 6;
-		for (auto el : v) {
-			cout << el << " ";
-
-		}
-		cout << "\n\n\n";
-	}*/
 }
